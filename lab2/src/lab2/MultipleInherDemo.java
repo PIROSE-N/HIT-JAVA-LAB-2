138c6a34116e8de01ecb6c8ea6c8b1755681091d
ELCOT <ELCOT@Lenovo>
2021-06-23 23:32:35 +0530

    JUST TRY IT

---
+++ lab2/src/lab2/MultipleInherDemo.java
@@ -0,0 +1,24 @@
+package lab2;
+
+public class MultipleInherDemo {
+	public static void main(String[] args) {
+		Pirose obj=new Pirose();
+		
+	}
+
+}
+class LivingObject{
+	
+}
+class Human extends LivingObject{
+	
+}
+class Dracula{
+	
+}
+class Dog{
+	
+}
+class Pirose extends Human{
+	
+}
